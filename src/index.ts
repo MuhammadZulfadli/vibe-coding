17046aa285ef546d6a71138402983b25fe9301d1
MuhammadZulfadly <zulfadlisimatupang@ecampus.ut.ac.id>
2026-04-07 11:59:18 +0700

    feat: integrate swagger API documentation

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
 import { Elysia } from "elysia";
+import { swagger } from "@elysiajs/swagger";
 import { usersRoute } from "./routes/users-route";
 
 export const app = new Elysia()
+  .use(swagger({
+    path: '/swagger',
+    documentation: {
+      info: {
+        title: 'Belajar Vibe Coding API',
+        version: '1.0.0',
+        description: 'Authentication API endpoints documentation'
+      }
+    }
+  }))
   .get("/", () => "Hello World")
   .use(usersRoute)
   .listen(3000);
